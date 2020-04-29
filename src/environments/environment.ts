0c222bc177ee5f2072c6a92c3298c1513946601c
Kazumasa Tsutsumi <k.tsutsumi0527@gmail.com>
2020-04-29 21:13:39 +0900

    feat(Angularfire): AngularFireの導入 #5

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCTU8W1MyKf6xFL5Ve-KxJj8DR_Tut3r7Y',
+    authDomain: 'encounter-management-a38d1.firebaseapp.com',
+    databaseURL: 'https://encounter-management-a38d1.firebaseio.com',
+    projectId: 'encounter-management-a38d1',
+    storageBucket: 'encounter-management-a38d1.appspot.com',
+    messagingSenderId: '534449943286',
+    appId: '1:534449943286:web:21322803e43fdf48e972d7',
+    measurementId: 'G-QGGZLS57WF',
+  },
 };
 
 /*
