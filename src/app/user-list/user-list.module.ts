6c8f1d2f2dbd616d044ffc42fe4edb183a3c5db4
Kazumasa Tsutsumi <k.tsutsumi0527@gmail.com>
2020-05-08 17:34:47 +0900

    fix(user-list): リスト一覧画面の作成 #18

--- src/app/user-list/user-list.module.ts
+++ src/app/user-list/user-list.module.ts
@@ -4,9 +4,17 @@ import { CommonModule } from '@angular/common';
 import { UserListRoutingModule } from './user-list-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [UserListComponent],
-  imports: [CommonModule, UserListRoutingModule, MatCardModule],
+  imports: [
+    CommonModule,
+    UserListRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTabsModule,
+  ],
 })
 export class UserListModule {}
--- src/app/user-list/user-list/user-list.component.ts
+++ src/app/user-list/user-list/user-list.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent implements OnInit {
+  users = [
+    {
+      name: 'まさみ3000',
+      nickName: 'まさみちゃん',
+      birthday: '19870603',
+      job: '女優',
+    },
+    {
+      name: '恵子',
+      nickName: '恵子ちゃん',
+      birthday: '19990909',
+      job: 'OL',
+    },
+    {
+      name: '花子',
+      nickName: 'トイレ',
+      birthday: '19990404',
+      job: '幽霊',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
