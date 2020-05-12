180440bcb14b3f0d7e57008972457a3bcd7fd433
Kazumasa Tsutsumi <k.tsutsumi0527@gmail.com>
2020-05-12 11:34:31 +0900

    feat(user-detail): リスト詳細ページの作成 #19

--- src/app/user-list/user-list-routing.module.ts
+++ src/app/user-list/user-list-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
     component: UserListComponent,
+  },
+  {
+    path: ':id',
+    component: UserDetailComponent,
   },
 ];
 
--- src/app/user-list/user-list.module.ts
+++ src/app/user-list/user-list.module.ts
@@ -6,15 +6,24 @@ import { UserListComponent } from './user-list/user-list.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [UserListComponent],
+  declarations: [UserListComponent, UserDetailComponent],
   imports: [
     CommonModule,
     UserListRoutingModule,
     MatCardModule,
     MatButtonModule,
     MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
   ],
 })
 export class UserListModule {}
