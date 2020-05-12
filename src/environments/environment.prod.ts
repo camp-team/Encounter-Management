24961ea870a3e545da6523953500667c2d5d8d05
Kazumasa Tsutsumi <k.tsutsumi0527@gmail.com>
2020-05-12 19:49:27 +0900

    fix(GitHub Actions): GitHub Actionsの修正 #55

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCTU8W1MyKf6xFL5Ve-KxJj8DR_Tut3r7Y',
+    authDomain: 'encounter-management-a38d1.firebaseapp.com',
+    databaseURL: 'https://encounter-management-a38d1.firebaseio.com',
+    projectId: 'encounter-management-a38d1',
+    storageBucket: 'encounter-management-a38d1.appspot.com',
+    messagingSenderId: '534449943286',
+    appId: '1:534449943286:web:21322803e43fdf48e972d7',
+    measurementId: 'G-QGGZLS57WF',
+  },
 };
