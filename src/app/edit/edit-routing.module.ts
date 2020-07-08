8c3b5698086eba56a90abd0cce9876ffd5ebfd23
Kazumasa Tsutsumi <k.tsutsumi0527@gmail.com>
2020-07-08 16:45:39 +0900

    feat(edit): 編集・新規作成画面の作成

---
+++ src/app/edit/edit-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EditComponent } from './edit/edit.component';
+import { FormGuard } from '../guards/form.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: EditComponent,
+    canDeactivate: [FormGuard],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class EditRoutingModule {}
